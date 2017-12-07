46c7f35b0e54f613e13bd3f403df992f0ce8d995
Kla Haeck <kla.haeck@gmail.com>
2017-12-07 15:01:43 -0600

    first commit

---
+++ src/Warp.js
@@ -0,0 +1,168 @@
+import { shapesToPaths, preparePaths } from './svg/normalize'
+import { getProperty, setProperty } from './svg/utils'
+import pathParser from './path/parser'
+import pathEncoder from './path/encoder'
+import { euclideanDistance } from './path/interpolate'
+import warpTransform from './warp/transform'
+import warpInterpolate from './warp/interpolate'
+import warpExtrapolate from './warp/extrapolate'
+
+export default class Warp
+{
+	constructor(element, curveType='q')
+	{
+		this.element = element
+
+		shapesToPaths(this.element)
+		preparePaths(this.element, curveType)
+
+		const pathElements = element.querySelectorAll('path')
+
+		this.paths = [].map.call(pathElements, function(element)
+		{
+			const pathString = getProperty(element, 'd')
+			const data = pathParser(pathString)
+
+			return { element, data }
+		})
+	}
+
+	update()
+	{
+		for(let path of this.paths)
+		{
+			const pathString = pathEncoder(path.data)
+			setProperty(path.element, 'd', pathString)
+		}
+	}
+
+	transform(transformer)
+	{
+		for(let path of this.paths)
+		{
+			path.data = warpTransform(path.data, transformer)
+		}
+
+		this.update()
+	}
+
+	interpolate(threshold)
+	{
+		let didWork = false
+
+		function deltaFunction(points)
+		{
+			const linearPoints = [
+				points[0].slice(0, 2),
+				points[points.length - 1].slice(0, 2),
+			]
+
+			const delta = euclideanDistance(linearPoints)
+			didWork = didWork || (delta > threshold)
+
+			return delta
+		}
+
+		for(let path of this.paths)
+		{
+			path.data = warpInterpolate(path.data, threshold, deltaFunction)
+		}
+
+		return didWork
+	}
+
+	extrapolate(threshold)
+	{
+		let didWork = false
+
+		function deltaFunction(points)
+		{
+			const linearPoints = [
+				points[0].slice(0, 2),
+				points[points.length - 1].slice(0, 2),
+			]
+
+			const delta = euclideanDistance(linearPoints)
+			didWork = didWork || (delta <= threshold)
+
+			return delta
+		}
+
+		for(let path of this.paths)
+		{
+			path.data = warpExtrapolate(path.data, threshold, deltaFunction)
+		}
+
+		return didWork
+	}
+
+	preInterpolate(transformer, threshold)
+	{
+		let didWork = false
+
+		function deltaFunction(points)
+		{
+			const linearPoints = [
+				points[0].slice(0, 2),
+				points[points.length - 1].slice(0, 2),
+			]
+
+			const delta = euclideanDistance(linearPoints)
+			didWork = didWork || (delta > threshold)
+
+			return delta
+		}
+
+		for(let path of this.paths)
+		{
+			const transformed = warpTransform(path.data, function(points)
+			{
+				const newPoints = transformer(points.slice(0, 2))
+				newPoints.push(...points)
+
+				return newPoints
+			})
+
+			const interpolated = warpInterpolate(transformed, threshold, deltaFunction)
+
+			path.data = warpTransform(interpolated, points => points.slice(2))
+		}
+
+		return didWork
+	}
+
+	preExtrapolate(transformer, threshold)
+	{
+		let didWork = false
+
+		function deltaFunction(points)
+		{
+			const linearPoints = [
+				points[0].slice(0, 2),
+				points[points.length - 1].slice(0, 2),
+			]
+			
+			const delta = euclideanDistance(linearPoints)
+			didWork = didWork || (delta <= threshold)
+
+			return delta
+		}
+
+		for(let path of this.paths)
+		{
+			const transformed = warpTransform(path.data, function(points)
+			{
+				const newPoints = transformer(points.slice(0, 2))
+				newPoints.push(...points)
+
+				return newPoints
+			})
+
+			const extrapolated = warpExtrapolate(transformed, threshold, deltaFunction)
+
+			path.data = warpTransform(extrapolated, points => points.slice(2))
+		}
+
+		return didWork
+	}
+}
---
+++ src/svg/normalize.js
@@ -0,0 +1,113 @@
+import pathParser from '../path/parser'
+import pathEncoder from '../path/encoder'
+import pathTransform from '../path/transform'
+import absoluteTransformer from '../path/transformers/absolute'
+import shortToLongTransformer from '../path/transformers/short-to-long'
+import hvzToLineTransformer from '../path/transformers/hvz-to-line'
+import lineToCurveTransformer from '../path/transformers/line-to-curve'
+import arcToCurveTransformer from '../path/transformers/arc-to-curve'
+import * as pathShape from '../path/shape'
+import { createElement, getProperty, setProperty } from './utils'
+
+export function shapesToPaths(element)
+{
+	const shapeElements = element.querySelectorAll('line, polyline, polygon, rect, ellipse, circle')
+
+	for(let shapeElement of shapeElements)
+	{
+		let path = {}
+		switch(shapeElement.tagName.toLowerCase())
+		{
+			case 'line':
+			{
+				path = pathShape.line(
+					getProperty(shapeElement, 'x1'),
+					getProperty(shapeElement, 'y1'),
+					getProperty(shapeElement, 'x2'),
+					getProperty(shapeElement, 'y2')
+				)
+			}
+			break
+			case 'polyline':
+			{
+				path = pathShape.polyline(...shapeElement.points)
+			}
+			break
+			case 'polygon':
+			{
+				path = pathShape.polygon(...shapeElement.points)
+			}
+			break
+			case 'rect':
+			{
+				path = pathShape.rectangle(
+					getProperty(shapeElement, 'x'),
+					getProperty(shapeElement, 'y'),
+					getProperty(shapeElement, 'width'),
+					getProperty(shapeElement, 'height'),
+					getProperty(shapeElement, 'rx'),
+					getProperty(shapeElement, 'ry')
+				)
+			}
+			break
+			case 'ellipse':
+			{
+				path = pathShape.ellipse(
+					getProperty(shapeElement, 'cx'),
+					getProperty(shapeElement, 'cy'),
+					getProperty(shapeElement, 'rx'),
+					getProperty(shapeElement, 'ry')
+				)
+			}
+			break
+			case 'circle':
+			{
+				path = pathShape.circle(
+					getProperty(shapeElement, 'cx'),
+					getProperty(shapeElement, 'cy'),
+					getProperty(shapeElement, 'r')
+				)
+			}
+			break
+		}
+
+		const pathString = pathEncoder(path)
+		const attributes = { d: pathString }
+
+		for(let attribute of shapeElement.attributes)
+		{
+			const name = attribute.nodeName
+			const value = attribute.nodeValue
+
+			// Avoid dimensional properties
+			if(!/^(x|y|x1|y1|x2|y2|width|height|r|rx|ry|cx|cy|points|d)$/.test(name))
+			{
+				attributes[name] = value
+			}
+		}
+
+		const pathElement = createElement('path', attributes)
+		shapeElement.parentNode.replaceChild(pathElement, shapeElement)
+	}
+}
+
+export function preparePaths(element, curveType='q')
+{
+	const pathElements = element.querySelectorAll('path')
+
+	for(let pathElement of pathElements)
+	{
+		let pathString = getProperty(pathElement, 'd')
+		let path = pathParser(pathString)
+
+		path = pathTransform(path, absoluteTransformer())
+		path = pathTransform(path, shortToLongTransformer())
+		path = pathTransform(path, hvzToLineTransformer())
+		path = pathTransform(path, lineToCurveTransformer(curveType))
+		path = pathTransform(path, arcToCurveTransformer())
+		
+		pathString = pathEncoder(path)
+
+		setProperty(pathElement, 'd', pathString)
+	}
+}
